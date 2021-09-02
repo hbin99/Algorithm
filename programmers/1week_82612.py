8f813aaeaf539235648ae1f155d6840a78574770
hbin <hyebin9612@gmail.com>
2021-09-02 20:13:08 +0900

    programmers commit

---
+++ programmers/1week_82612.py
@@ -0,0 +1,12 @@
+def solution(price, money, count):
+    total = 0;
+
+    for i in range(1, count + 1):
+        total += price * i;
+
+    if money > total:
+        return 0;
+
+    answer = total - money;
+
+    return answer;
